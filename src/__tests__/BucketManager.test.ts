21becf6093e67104647308056a6566129ef4371f
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-02-16 09:59:59 +0100

    Create BucketManager tests

---
+++ src/__tests__/BucketManager.test.ts
@@ -0,0 +1,84 @@
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+beforeEach(() => {
+
+});
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function
+afterEach(async () => {
+
+});
+
+describe("CloudStorageBucketManager unit tests", () => {
+  test("createObject_CreateNewBucket_Success", async ()=>{
+    //given
+
+    //when
+
+    //then
+  });
+
+  test("CreateObject_CreateObjectWithExistingBucket_Success", async ()=>{
+    //given
+
+    //when
+
+    //then
+  });
+
+  test("CreateObject_CreateObjectBucketNotExist_Success", async ()=>{
+    //given
+
+    //when
+
+    //then
+  });
+
+  test("DownloadObject_NominalCase_Success", async ()=>{
+    //given
+
+    //when
+
+    //then
+  });
+
+  test("IsObjectExists_NominalCase_Success", async ()=>{
+    //given
+
+    //when
+
+    //then
+  });
+
+  test("IsObjectExists_ObjectNotExistBucket_Success", async ()=>{
+    //given
+
+    //when
+
+    //then
+  });
+
+  test("IsObjectExists_ObjectNotExistFile_Success", async ()=>{
+    //given
+
+    //when
+
+    //then
+  });
+
+  test("RemoveObject_EmptyBucket_Success", async ()=>{
+    //given
+
+    //when
+
+    //then
+  });
+
+  test("RemoveObject_NotEmptyBucket_Success", async ()=>{
+    //given
+
+    //when
+
+    //then
+  });
+});
