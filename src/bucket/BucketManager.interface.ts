b2e1acb2e84766c23f34a56f5d9099d13b0b1449
AlexandrePhilibert <philibert.alexandre@gmail.com>
2022-03-02 08:58:47 +0100

    Make BucletManager createObject return a Promise

--- src/bucket/BucketManager.interface.ts
+++ src/bucket/BucketManager.interface.ts
@@ -1,5 +1,5 @@
 export interface BucketManager {
-  createObject(objectUrl: string, filePath?: string): void;
+  createObject(objectUrl: string, filePath?: string): Promise<void>;
 
   objectExists(objectUrl: string): Promise<boolean>;
 
