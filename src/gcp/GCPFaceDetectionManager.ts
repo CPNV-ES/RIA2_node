c55a561a4d317547c5aa50b85117680a770d3689
AlexandrePhilibert <philibert.alexandre@gmail.com>
2022-03-09 09:52:39 +0100

    Implement detectFaces method

---
+++ src/gcp/GCPFaceDetectionManager.ts
@@ -0,0 +1,21 @@
+import vision, { ImageAnnotatorClient } from "@google-cloud/vision";
+
+import { FaceDetectionManager } from "src/interfaces/FaceDetectionManager.interface";
+
+export class GCPFaceDetectionManager implements FaceDetectionManager {
+  client: ImageAnnotatorClient;
+
+  constructor() {
+    this.client = new vision.ImageAnnotatorClient();
+  }
+
+  async detectFaces(imageUrl: string): Promise<any> {
+    const results = await this.client.faceDetection({
+      image: { source: { filename: imageUrl } },
+    });
+    const faces = results[0].faceAnnotations;
+    const numFaces = faces?.length;
+    console.log(`Found ${numFaces} face${numFaces === 1 ? "" : "s"}.`);
+    return faces;
+  }
+}
