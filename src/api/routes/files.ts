87966ad4e762749e800fa0281db223340c533277
Yvann <yvann.butticaz@gmail.com>
2022-03-10 12:04:02 +0100

    Create bucket on file upload & store the file
    Co-authored-by: AlexandrePhilibertCPNV <AlexandrePhilibertCPNV@users.noreply.github.com>

--- src/api/routes/files.ts
+++ src/api/routes/files.ts
@@ -1,15 +1,23 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
 
+import { GCPBucketManager } from './../../gcp/GCPBucketManager';
+
 const router = express.Router();
 const upload = multer({ dest: process.env.FILE_UPLOAD_PATH });
 
-router.post("/", upload.single("file"), (req: Request, res: Response) => {
+router.post("/", upload.single("file"), async (req: Request, res: Response) => {
   if (!req.file) {
     return res.status(400).json({
       error: "No file uploaded",
     });
   }
+
+  const bucketManager = new GCPBucketManager();
+  const bucketName = req.file.filename;
+
+  await bucketManager.createObject(bucketName);
+  await bucketManager.createObject(bucketName, req.file.path);
 
   res.status(201).json({
     filename: req.file.filename,
