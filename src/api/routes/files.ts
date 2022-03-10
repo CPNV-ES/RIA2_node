b5cab477bf54ac713b4e28d9bd9b41b0033d0ed5
AlexandrePhilibert <philibert.alexandre@gmail.com>
2022-03-10 11:14:12 +0100

    Analyze image from google storage

--- src/api/routes/files.ts
+++ src/api/routes/files.ts
@@ -1,19 +1,25 @@
 import express, { Request, Response } from "express";
 import multer from "multer";
 
+import { GCPFaceDetectionManager } from "../../gcp/GCPFaceDetectionManager";
+
 const router = express.Router();
 const upload = multer({ dest: process.env.FILE_UPLOAD_PATH });
 
-router.post("/", upload.single("file"), (req: Request, res: Response) => {
+router.post("/", upload.single("file"), async (req: Request, res: Response) => {
   if (!req.file) {
     return res.status(400).json({
       error: "No file uploaded",
     });
   }
 
-  res.status(201).json({
-    filename: req.file.filename,
-  });
+  const faceDetectionManager = new GCPFaceDetectionManager();
+
+  const result = await faceDetectionManager.detectFaces(
+    "gs://ria2nodejs.actualit.info/alexandre.jpg",
+  );
+
+  res.status(201).json(result);
 });
 
 export default router;
