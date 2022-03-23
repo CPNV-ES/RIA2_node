f058caeffd6f09e504c365f39778daa86b687e50
AndiSO-1 <andi.santos-oliveira@cpnv.ch>
2022-03-23 10:20:09 +0100

    Edit upload to use the uploaded img for detection

--- src/gcp/GCPBucketManager.ts
+++ src/gcp/GCPBucketManager.ts
@@ -10,24 +10,20 @@ export class GCPBucketManager implements BucketManager {
   }
 
   async createObject(objectUrl: string, filePath?: string): Promise<void> {
-    const fileName = this.getImgName(objectUrl);
-
     // If the bucket didn't exist creates it
-    if (!await this.objectExists(this.removeFileNameFromUrl(objectUrl)))
-    {
-      const [bucket, apiResponse] = await this.storage.createBucket(this.getBucketNameFromUrl(objectUrl));
+    if (!(await this.objectExists(this.removeFileNameFromUrl(objectUrl)))) {
+      const [bucket, apiResponse] = await this.storage.createBucket(
+        this.getBucketNameFromUrl(objectUrl),
+      );
       // If there is a file create it
-      if (fileName) {
-        await bucket.upload(filePath || '');
+      if (filePath) {
+        await bucket.upload(filePath || "");
       }
     }
-    else
-    {
-      // If there is a file create it
-      if (fileName) {
-        const bucket = this.storage.bucket(this.getBucketNameFromUrl(objectUrl));
-        await bucket.upload(filePath || '');
-      }
+    // Else if there is a file create it
+    else if (filePath) {
+      const bucket = this.storage.bucket(this.getBucketNameFromUrl(objectUrl));
+      await bucket.upload(filePath || "");
     }
   }
 
@@ -37,8 +33,7 @@ export class GCPBucketManager implements BucketManager {
 
     const [bucketExists] = await bucket.exists();
 
-    if (fileName)
-    {
+    if (fileName) {
       const file = bucket.file(fileName);
       const [fileExists] = await file.exists();
 
@@ -58,7 +53,10 @@ export class GCPBucketManager implements BucketManager {
     }
   }
 
-  async downloadObject(objectUrl: string, destinationUri: string): Promise<void> {
+  async downloadObject(
+    objectUrl: string,
+    destinationUri: string,
+  ): Promise<void> {
     const fileName = this.getImgName(objectUrl);
     const bucket = this.storage.bucket(this.getBucketNameFromUrl(objectUrl));
     const file = bucket.file(fileName);
@@ -68,21 +66,18 @@ export class GCPBucketManager implements BucketManager {
     });
   }
 
-  getImgName(url?: string) : string{
-    if (url)
-      return url.replace(/((?!\/\w*\.\w*$).)*/, '').replace(/\//, '');
+  getImgName(url?: string): string {
+    if (url) return url.replace(/((?!\/\w*\.\w*$).)*/, "").replace(/\//, "");
     return "";
   }
 
-  removeFileNameFromUrl(url?: string) : string{
-    if (url)
-      return url.replace(/\/\w*\.\w*$/, '');
+  removeFileNameFromUrl(url?: string): string {
+    if (url) return url.replace(/\/\w*\.\w*$/, "");
     return "";
   }
 
-  getBucketNameFromUrl(url?: string) : string{
-    if (url)
-      return this.removeFileNameFromUrl(url.replace(/^gs:\/\//, ''));
+  getBucketNameFromUrl(url?: string): string {
+    if (url) return this.removeFileNameFromUrl(url.replace(/^gs:\/\//, ""));
     return "";
   }
 }
