28e71b2035d7447312bcf1ab7d76a8c948ebf6cb
AlexandrePhilibert <philibert.alexandre@gmail.com>
2022-03-30 08:35:43 +0200

    Rename uploaded image from file to img

--- src/api/routes/image-analysis.ts
+++ src/api/routes/image-analysis.ts
@@ -7,7 +7,7 @@ import { GCPFaceDetectionManager } from "../../gcp/GCPFaceDetectionManager";
 const router = express.Router();
 const upload = multer({ dest: process.env.FILE_UPLOAD_PATH });
 
-router.post("/", upload.single("file"), async (req: Request, res: Response) => {
+router.post("/", upload.single("img"), async (req: Request, res: Response) => {
   if (!req.file) {
     return res.status(400).json({
       error: "No file uploaded",
